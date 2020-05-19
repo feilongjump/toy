9b4245e0775cd48bf78fa4337aea5c7f4872c8cf
pretendtrue <pretendtrue@qq.com>
2020-05-19 15:09:11 +0800

    :tada: 你好，初次见面！

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": resolve("src")
+      }
+    }
+  },
+  devServer: {
+    hot: true,
+    open: true
+  }
+};
