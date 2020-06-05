acdcc310bf691adaf14a11cbf57cbe72a18c50d1
pretendtrue <pretendtrue@qq.com>
2020-06-05 17:47:08 +0800

    首页

--- src/pages/index/views/index/components/Details.vue
+++ src/pages/index/views/index/components/Details.vue
@@ -1,7 +1,52 @@
 <template>
-  <el-row id="details" type="flex" justify="center" align="middle">
-    <el-col class="text-center">
-      <span>生命在于折腾</span>
+  <el-row id="details" class="overflow-y-scroll hide-scrollbar">
+    <el-col>
+      <mavonEditor
+        v-model="content.markdown"
+        :subfield="false"
+        :boxShadow="false"
+        defaultOpen="preview"
+        previewBackground="#fff"
+        :toolbarsFlag="false"
+      />
     </el-col>
   </el-row>
 </template>
+
+<script>
+import { show } from '@/api/article.js'
+import { mavonEditor } from "mavon-editor";
+import 'mavon-editor/dist/css/index.css';
+
+export default {
+  props: {
+    id: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      content: {
+        markdown: '# 生命在于折腾'
+      }
+    }
+  },
+  components: {
+    mavonEditor,
+  },
+  watch: {
+    id () {
+      show(this.id).then(response => {
+        this.content = response.content
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.v-note-wrapper,.v-note-panel {
+  border: none !important;
+}
+</style>
--- src/pages/index/views/index/home.vue
+++ src/pages/index/views/index/home.vue
@@ -1,49 +1,76 @@
 <template>
   <el-row class="h-screen text-white py-4 px-4">
-    <el-col :md="12" :lg="14">
+    <el-col :md="12" :lg="14" class="h-full overflow-y-scroll hide-scrollbar" v-infinite-scroll="getArticles">
       <i class="el-icon-more-outline text-2xl mt-4 mb-8"></i>
       <div class="px-8">
         <el-row class="ml-3 my-8">
           <el-col :xs="24" :sm="10" :md="12" :lg="8">
-            <el-input class="index-search text-2xl" v-model="keywords" placeholder="Search"></el-input>
+            <el-input class="index-search text-2xl" v-model="listQuery.keywords" placeholder="Search"></el-input>
           </el-col>
         </el-row>
         <el-row class="ml-3 mt-8">
           <h2>Articles</h2>
-          <el-col>
-            <h3>Title</h3>
-            <p class="text-indent-4">
-              Tailwind CSS 是一个高度可定制的基础层 CSS 框架，它为您提供了构建定制化设计所需的所有构建块，而无需重新覆盖任何内建于框架中的设计风格。
-            </p>
-            <el-row>
-              <el-col :span="18">
-                <span class="tag"><i class="el-icon-price-tag mr-1"></i>Tag</span>
-              </el-col>
-              <el-col :span="6">
-                <span>2020-05-21 17:10</span>
-              </el-col>
-            </el-row>
+          <el-col v-for="article in articles" :key="article.id">
+            <h3 class="cursor-pointer" @click="transmitDetailsId(article.id)">{{article.title}}</h3>
           </el-col>
         </el-row>
       </div>
     </el-col>
     <el-col class="h-full hidden-sm-and-down text-black" :md="12" :lg="10">
-      <Details />
+      <Details :id="id" />
     </el-col>
   </el-row>
 </template>
 
 <script>
+import { index } from '@/api/article.js'
 import Details from "./components/Details";
+import { isEmpty } from 'lodash'
 
 export default {
   data () {
     return {
-      keywords: ''
+      id: 0,
+      articles: [],
+      meta: {},
+      listQuery: {
+        keywords: '',
+        page: 0,
+        limit: 20
+      }
     }
   },
   components: {
     Details
+  },
+  created () {
+    this.getArticles()
+  },
+  methods: {
+    /**
+     * 文章列表
+     */
+    getArticles() {
+
+      if (! isEmpty(this.meta) && this.meta.last_page === this.listQuery.page) {
+        return true;
+      }
+      this.listQuery.page++
+
+      index(this.listQuery).then(response => {
+        this.articles = this.articles.concat(response.data)
+        this.meta = response.meta
+      })
+    },
+    /**
+     * 传递文章 id
+     */
+    transmitDetailsId (id) {
+      this.id = id
+    },
+    test() {
+      console.info( this.listQuery)
+    }
   }
 }
 </script>
