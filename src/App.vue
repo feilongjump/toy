1ae5c3b12f731072c683eefd8780441ee6303ca1
pretendtrue <pretendtrue@qq.com>
2020-11-13 14:06:12 +0800

    Main 增加 Navbar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="w-full h-screen bg-main-pattern p-8 font-mono flex justify-start">
     <Sidebar />
 
-    <main class="ml-8 py-4">
+    <main class="ml-8 pb-4 w-full">
+      <Navbar />
       <router-view />
     </main>
   </div>
@@ -11,11 +12,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Sidebar from './components/Sidebar.vue'
+import Navbar from './components/Navbar.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     Sidebar,
+    Navbar,
   },
 })
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="h-16 w-full py-4 flex justify-end items-center">
+    <span class="font-semibold">
+      有新消息！
+    </span>
+  </header>
+</template>
