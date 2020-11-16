aa69e52b3903552029ac658ba962fd9a5aa73447
pretendtrue <pretendtrue@qq.com>
2020-11-16 11:53:58 +0800

    发表文章

--- src/views/article/Create.vue
+++ src/views/article/Create.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
+  <div class="h-full">
+    <div class="mb-8">
+      <input
+        type="text"
+        class="w-64 outline-none bg-transparent text-black pl-4 py-1 border-b-2 border-solid border-pink-500"
+        placeholder="给它起个名字吧"
+      />
+      <button class="ml-2">记录一下吧</button>
+    </div>
     <div id="vditor" class="vditor"></div>
   </div>
 </template>
@@ -14,7 +22,7 @@ export default defineComponent({
     onMounted(() => {
       new Vditor('vditor', {
         height: 360,
-        theme: 'material',
+        theme: 'classic',
         cache: {
           enable: false,
         },
