e00472e3fd4258008a179d974924f6e6cc4ec5d8
PretendTrue <pretendtrue@qq.com>
2020-11-17 21:13:16 +0800

    修改主页面结构

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />
+  <div class="w-full h-screen bg-main-pattern px-12 py-8 font-mono">
+    <div
+      class="w-full h-full border border-white rounded-2xl p-4 shadow-2xl bg-white bg-opacity-25"
+    >
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- src/layouts/Index.vue
+++ src/layouts/Index.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="w-full h-screen bg-main-pattern px-12 py-8 font-mono">
-    <div
-      class="w-full h-full border border-white rounded-2xl p-4 flex justify-start shadow-2xl bg-white bg-opacity-25"
-    >
-      <Sidebar />
+  <div class="w-full h-full flex justify-start">
+    <Sidebar />
 
-      <main class="ml-8 w-full flex flex-col">
-        <Navbar />
-        <router-view />
-      </main>
-    </div>
+    <main class="ml-8 w-full flex flex-col">
+      <Navbar />
+      <router-view />
+    </main>
   </div>
 </template>
 
