7f6ab6a3e4f18f37b61b4b8a2b7852a77996bb8f
pretendtrue <pretendtrue@qq.com>
2020-12-25 11:23:44 +0800

    :construction: 自定义滚动条

--- src/layouts/Index.vue
+++ src/layouts/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-full min-h-screen px-4 pt-8">
+    <Scrollbar />
     <main class="lg:w-1/2 mx-auto flex flex-col">
       <router-view />
     </main>
@@ -8,8 +9,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Scrollbar from './components/Scrollbar.vue'
 
 export default defineComponent({
   name: 'Layout',
+  components: {
+    Scrollbar,
+  },
 })
 </script>
