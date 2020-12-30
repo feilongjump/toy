f2c3e4624c8982752b475c2c43e2beca72598e4b
pretendtrue <pretendtrue@qq.com>
2020-12-30 14:11:21 +0800

    :sparkles: 监听回车键之登录操作

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,20 +7,24 @@
       type="text"
       class="w-64 outline-none bg-transparent text-black pl-4 py-1 border-b-2 border-solid border-pink-500"
       placeholder="账号名还是邮箱？"
+      ref="accountDom"
+      @keyup.enter="keyup"
       v-model="username"
     />
     <input
       type="password"
       class="w-64 outline-none bg-transparent text-black pl-4 py-1 border-b-2 border-solid border-pink-500 mx-4 my-6"
       placeholder="密码是什么"
+      ref="passwordDom"
+      @keyup.enter="keyup"
       v-model="password"
     />
-    <Icon href="#icon-login" :className="['cursor-pointer']" @click="login" />
+    <Icon href="#icon-login" :className="['cursor-pointer']" @click="keyup" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue'
+import { defineComponent, ref, reactive, toRefs, onMounted, Ref } from 'vue'
 import Icon from '@/components/Icon.vue'
 import Auth from '@/api/auth'
 import { useRouter } from 'vue-router'
@@ -30,16 +34,15 @@ export default defineComponent({
   name: 'Login',
   setup() {
     const router = useRouter()
-    const data = reactive({
+    const accountDom = ref<HTMLElement>()
+    const passwordDom = ref<HTMLElement>()
+    const params = reactive({
       username: '',
       password: '',
     })
-    const login = async () => {
-      const params = {
-        username: data.username,
-        password: data.password,
-      }
 
+    // 用户登录
+    const login = async () => {
       await new Auth().login(params).then((response: any) => {
         localStorage.setItem('token_type', response.token_type)
         localStorage.setItem('token', response.access_token)
@@ -52,11 +55,38 @@ export default defineComponent({
       })
     }
 
+    // dom 元素选中
+    const domFocus = (dom: Ref<HTMLElement | undefined>) => {
+      if (dom.value) {
+        dom.value.focus()
+      }
+    }
+
+    // 回车键操作
+    const keyup = () => {
+      // 用户值为空时，选中用户输入框
+      if (params.username.length == 0) domFocus(accountDom)
+
+      // 密码值为空时，选中密码输入框
+      if (params.username.length > 0 && params.password.length == 0)
+        domFocus(passwordDom)
+
+      // 都存在值，则进行登录
+      if (params.username.length > 0 && params.password.length > 0) login()
+    }
+
+    onMounted(() => {
+      // 页面渲染后默认选中 account dom
+      domFocus(accountDom)
+    })
+
     localStorage.clear()
 
     return {
-      ...toRefs(data),
-      login,
+      ...toRefs(params),
+      keyup,
+      accountDom,
+      passwordDom,
     }
   },
   components: {
@@ -64,5 +94,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style></style>
