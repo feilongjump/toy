8582f53fc228c33b30e639728e1f6c7b0f8f1d2b
PretendTrue <pretendtrue@qq.com>
2021-01-06 20:11:15 +0800

    :wrench: 修改 eslint 配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-var-requires': ['off'],
     'prefer-const': 'off',
   },
 }
