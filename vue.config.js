3d054522bb9aadf8b33f10640b0a7662c011029e
PretendTrue <pretendtrue@qq.com>
2021-01-06 20:13:50 +0800

    :wrench: 使用 Gzip 压缩

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -5,5 +9,16 @@ module.exports = {
         plugins: [require('tailwindcss'), require('autoprefixer')],
       },
     },
+  },
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: productionGzipExtensions,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
   },
 }
