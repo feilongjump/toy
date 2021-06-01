9316c30fbf2f41bca49e11324b95e12dce06af5c
feilongjump <feilongjump@qq.com>
2021-06-01 17:46:37 +0800

    :tada: hello vite + vue3 + tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen w-full flex justify-center items-center">
+    <img class="h-36" alt="Vue logo" src="./assets/logo.png" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
