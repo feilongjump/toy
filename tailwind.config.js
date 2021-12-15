09033be45fb2dc842a43ed288de6fd0deebd4368
feilongjump <feilongjump@qq.com>
2021-12-15 18:25:07 +0800

    update tailwind css 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       blur: {
         '4xl': '96px',
         '5xl': '136px'
       }
-    }
-  },
-  variants: {
-    extend: {
-      margin: ['last']
     }
   },
   plugins: [require('@tailwindcss/typography')]
